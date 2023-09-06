074d1f00f6e778b9d2923d9e683613031d104585
DanielBerg <daniel.berg@l58.se>
2023-09-06 12:58:42 +0200

    add new project

---
+++ src/Web/SP.Web.Site/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SP.Web.Site.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ src/Web/SP.Web.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SP.Web.Site.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
